79575ea225f195c4642beb5d82de411d47c90fa4
nmaffly <ncmaffly@ucdavis.edu>
2024-02-25 22:04:41 -0800

    all recent spotify data extracted

--- app.py
+++ app.py
@@ -47,28 +47,27 @@ def display():
     spotify = spotipy.Spotify(auth=token_info['access_token'])
 
     user_data = spotify.current_user()
-    artist_genres_r = get_artist_genres(spotify) # dictionary with top 10 artists and associated genre
-    genres_r = get_genre_count(artist_genres_r)
+    top_artists_r = spotify.current_user_top_artists(limit=10, time_range='short_term')['items']
+    top_artists_a = spotify.current_user_top_artists(limit=10, time_range='long_term')['items']
+    top_tracks_r = spotify.current_user_top_tracks(limit=10, time_range='short_term')['items']
+    top_tracks_a = spotify.current_user_top_tracks(limit=10, time_range='long_term')['items']
 
-    popularity_r = get_popularity_r(spotify)
-
+    artist_genres_r = get_artist_genres(top_artists_r) # dictionary with top 10 artists and associated genre
     artists_r = list(artist_genres_r.keys())
+    genres_r = get_genre_count(artist_genres_r)
+    popularity_r = get_popularity(top_artists_r)
+    tempo_r, loudness_r, acousticness_r, danceability_r, valence_r, energy_r, speechiness_r = get_audio_features_tracks(top_tracks_r, spotify)
 
-    genres_a = get_all_time(spotify)
-    genre_count_a = get_genre_count(genres_a)
 
-        
+    return render_template('index.html', user_data=user_data, artist_genres_r=artist_genres_r, genre__r=genres_r,popularity_r=popularity_r, artists_r=artists_r, tempo_r=tempo_r, loudness_r=loudness_r, acousticness_r=acousticness_r, danceability_r=danceability_r, valence_r=valence_r, energy_r=energy_r, speechiness_r=speechiness_r)
 
-    return render_template('index.html', user_data=user_data, artist_genres_r=artist_genres_r, genre__r=genres_r, genres_a=genres_a, genre_count_a=genre_count_a, popularity_r=popularity_r, artists_r=artists_r)
-
-def get_artist_genres(spotify):
+def get_artist_genres(top_artists):
     # returns dictionary with top 10 artists and associated genres, popularity scores, 
     # dictionary: (key, value) --> (artist, genres)
     artists = {}
-    top_artists = spotify.current_user_top_artists(limit=10, time_range='short_term')['items']
     for item in top_artists:
         artists[item['name']] = item['genres']
-    
+
     return artists
 
 def get_genre_count(genres):
@@ -88,11 +87,9 @@ def get_genre_count(genres):
     
     return all_genres
 
-def get_popularity_r(spotify):
-    # returns nested dictionary with top 10 artists and associated genres, popularity scores, 
-    # dictionary: (key, value) --> (artist, genres)
+def get_popularity(top_artists):
+    # returns median popularity score for top 10 artists
     popularity_scores = []
-    top_artists = spotify.current_user_top_artists(limit=10, time_range='short_term')['items']
     for item in top_artists:
         popularity_scores.append(item['popularity'])
 
@@ -101,6 +98,153 @@ def get_popularity_r(spotify):
     median = find_median(popularity_scores)
     
     return median
+
+def get_audio_features_tracks(top_tracks, spotify):
+    # returns median tempo for top 10 artists
+    tempo_scores = []
+    loudness_scores = []
+    acousticness_scores = []
+    danceability_scores = []
+    valence_scores = []
+    energy_scores = []
+    speechiness_scores = []
+    
+    for track in top_tracks:
+        # print(artist['name'], end=':\n')
+        tempo = 0
+        loudness = 0
+        acousticness = 0
+        danceability = 0
+        valence = 0
+        energy = 0
+        speechiness = 0
+
+        if track:
+            track_id = track['id']
+            audio_features = spotify.audio_features([track_id])[0]
+            if 'tempo' in audio_features:
+                tempo = audio_features['tempo']
+
+            if 'loudness' in audio_features:
+                loudness += audio_features['loudness']
+
+            if 'acousticness' in audio_features:
+                #print(f"{track['name']} acousticness: {audio_features['acousticness']}")
+                acousticness += audio_features['acousticness']
+
+            if 'danceability' in audio_features:
+                danceability += audio_features['danceability']
+
+            if 'valence' in audio_features:
+                valence += audio_features['valence']
+
+            if 'energy' in audio_features:
+                energy += audio_features['energy']
+            
+            if 'speechiness' in audio_features:
+                speechiness += audio_features['speechiness']
+
+            # print(f"{track['name']} tempo: {audio_features['tempo']}")
+            # print(f"{track['name']} loudness: {audio_features['loudness']}")
+        
+
+        tempo_scores.append(tempo)
+        loudness_scores.append(loudness)
+        acousticness_scores.append(acousticness)
+        danceability_scores.append(danceability)
+        valence_scores.append(valence)
+        energy_scores.append(energy)
+        speechiness_scores.append(speechiness)
+
+    median_tempo = round(find_median(tempo_scores),ndigits=2)
+    median_loudness = round(find_median(loudness_scores),ndigits=2)
+    median_acousticness = round(find_median(acousticness_scores), ndigits=2)
+    median_danceability = round(find_median(danceability_scores), ndigits=2)
+    median_valence = round(find_median(valence_scores), ndigits=2)
+    median_energy = round(find_median(energy_scores), ndigits=2)
+    median_speechiness = round(find_median(speechiness_scores), ndigits=2)
+    
+    return median_tempo, median_loudness, median_acousticness, median_danceability, median_valence, median_energy, median_speechiness
+
+def get_audio_features_artists(top_artists, spotify):
+    # returns median tempo for top 10 artists
+    tempo_scores = []
+    loudness_scores = []
+    acousticness_scores = []
+    danceability_scores = []
+    valence_scores = []
+    energy_scores = []
+    speechiness_scores = []
+    
+    for artist in top_artists:
+        top_tracks = spotify.artist_top_tracks(artist['id'], country='US')['tracks']
+        # print(artist['name'], end=':\n')
+
+        if top_tracks:
+            avg_tempo = 0
+            avg_loudness = 0
+            avg_acousticness = 0
+            avg_danceability = 0
+            avg_valence = 0
+            avg_energy = 0
+            avg_speechiness = 0
+            num_tracks = 0
+            for track in top_tracks:
+                track_id = track['id']
+                audio_features = spotify.audio_features([track_id])[0]
+                if audio_features['tempo']:
+                    avg_tempo += audio_features['tempo']
+
+                if audio_features['loudness']:
+                    avg_loudness += audio_features['loudness']
+
+                if 'acousticness' in audio_features:
+                    #print(f"{track['name']} acousticness: {audio_features['acousticness']}")
+                    avg_acousticness += audio_features['acousticness']
+
+                if 'danceability' in audio_features:
+                    avg_danceability += audio_features['danceability']
+
+                if 'valence' in audio_features:
+                    avg_valence += audio_features['valence']
+
+                if 'energy' in audio_features:
+                    avg_energy += audio_features['energy']
+                
+                if 'speechiness' in audio_features:
+                    avg_speechiness += audio_features['speechiness']
+
+                # print(f"{track['name']} tempo: {audio_features['tempo']}")
+                # print(f"{track['name']} loudness: {audio_features['loudness']}")
+                
+                num_tracks += 1
+            
+            avg_tempo /= num_tracks
+            avg_loudness /= num_tracks
+            avg_acousticness /= num_tracks
+            avg_danceability /= num_tracks
+            avg_valence /= num_tracks
+            avg_energy /= num_tracks
+            avg_speechiness /= num_tracks
+        
+
+        tempo_scores.append(avg_tempo)
+        loudness_scores.append(avg_loudness)
+        acousticness_scores.append(avg_acousticness)
+        danceability_scores.append(avg_danceability)
+        valence_scores.append(avg_valence)
+        energy_scores.append(avg_energy)
+        speechiness_scores.append(avg_speechiness)
+
+    median_tempo = round(find_median(tempo_scores),ndigits=2)
+    median_loudness = round(find_median(loudness_scores),ndigits=2)
+    median_acousticness = round(find_median(acousticness_scores), ndigits=2)
+    median_danceability = round(find_median(danceability_scores), ndigits=2)
+    median_valence = round(find_median(valence_scores), ndigits=2)
+    median_energy = round(find_median(energy_scores), ndigits=2)
+    median_speechiness = round(find_median(speechiness_scores), ndigits=2)
+    
+    return median_tempo, median_loudness, median_acousticness, median_danceability, median_valence, median_energy, median_speechiness
 
 def find_median(numbers):
     numbers.sort()
@@ -111,25 +255,6 @@ def find_median(numbers):
         return numbers[middle]
     else:
         return (numbers[middle - 1] + numbers[middle]) / 2.0
-
-def get_medium(spotify):
-    artists = {}
-    top_artists = spotify.current_user_top_artists(limit=10, time_range='medium_term')['items']
-    for item in top_artists:
-        artists[item['name']] = item['genres']
-    
-    return artists
-
-def get_all_time(spotify):
-    artists = {}
-    top_artists = spotify.current_user_top_artists(limit=10, time_range='long_term')['items']
-    for item in top_artists:
-        artists[item['name']] = item['genres']
-    
-    return artists
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)

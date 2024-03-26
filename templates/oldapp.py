a451b72d9b0cca0dc3568618aee339892797bad6
Rishit Das <rdas@ucdavis.edu>
2024-03-27 08:26:17 +1100

    Working collab view

--- templates/oldapp.py
+++ templates/oldapp.py
@@ -5,17 +5,17 @@ from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 from extraction import get_artist_genres, get_genre_count, get_popularity, get_audio_features_tracks, get_variance
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta
+from datetime import datetime
 from flask_migrate import Migrate
 from flask_session import Session
-from database import User, UserStats, RecentGenres, AllTimeGenres, RecentArtists, AllTimeArtists, RecentTracks, AllTimeTracks, get_db_genres, get_db_artists, get_db_median_values, get_db_tracks, generate_sharing_token, load_user_stats, db
+from database import User, UserStats, RecentGenres, AllTimeGenres, RecentArtists, AllTimeArtists, RecentTracks, AllTimeTracks, get_db_genres, get_db_artists, get_db_median_values, get_db_tracks, generate_sharing_token, db
 
 app = Flask(__name__)
 
 # Configure the Flask app to use Flask-Session
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
-Session(app)  # Initialize the session
+Session(app) 
 
 import os
 
@@ -54,19 +54,7 @@ def callback():
     cleanup()
     token_info = sp_oauth.get_access_token(code)
     session['token_info'] = token_info
-    if not token_info:
-        return redirect('/login')
-    if sp_oauth.is_token_expired(token_info):
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-        session['token_info'] = token_info
-    spotify = spotipy.Spotify(auth=token_info['access_token'])
-    user_data = spotify.current_user()
-    user = User.query.filter_by(spotify_id=user_data['id']).first()
-
-    new_user = False
-    if user:
-        new_user = True
-    return render_template('loading.html', new_user = new_user)
+    return render_template('loading.html')
 
 @app.route('/fetch_data')
 def fetch_data():
@@ -81,17 +69,15 @@ def fetch_data():
 
     # Extracted and stored user data
     user_data = spotify.current_user()
-    print("Here_fetched")
+    
     # check if user exists
     user = User.query.filter_by(spotify_id=user_data['id']).first()
 
     print("User acquired from database")
 
-    if user: #I changed this just to work on things
-        # If user exists and it's been less than a week, update tokens and most recent login
+    if user:
+        # If user exists, update tokens and most recent login
         # then pull stats from database into flask session
-
-        print("It's been less than 30 seconds. No data update")
 
         user.access_token = token_info['access_token']
         user.refresh_token = token_info['refresh_token']
@@ -128,8 +114,8 @@ def fetch_data():
     }
 
     else:
-        # otherwise extract spotify info, add to database and session
-        print("User doesn't exist, or it's been longer than 30 seconds, new data being pulled")
+        # otherwise if it's a new user extract spotify info, add to database and session
+
         # stored user data
         user_name = user_data['display_name']
         profile_pic = user_data['images'][1]['url']
@@ -164,72 +150,139 @@ def fetch_data():
         median_values_a.append(popularity_a)
         median_values_a.append(variance_a)
 
-        new_data = {
-                "recent_genres": genres_r,
-                "all_time_genres": genres_a,
-                "recent_artists": artists_r,
-                "all_time_artists": artists_a,
-                "recent_tracks": tracks_r,
-                "all_time_tracks": tracks_a
-        }
+        # setting up db entry for new user
+        user_share_token = generate_sharing_token()
+        user = User(
+            spotify_id=user_data['id'],
+            display_name=user_data.get('display_name', 'No Name'),
+            profile_pic=user_data['images'][0]['url'] if user_data['images'] else None,
+            access_token=token_info['access_token'],
+            refresh_token=token_info['refresh_token'],
+            share_token=user_share_token,
+            token_expires_at=datetime.fromtimestamp(token_info['expires_at'])
+        )
 
-        if not user:
-            #if the user doesn't exist, create new user
-            # setting up db entry for new user
-            user_share_token = generate_sharing_token()
-            user = User(
-                spotify_id=user_data['id'],
-                display_name=user_data.get('display_name', 'No Name'),
-                profile_pic=user_data['images'][0]['url'] if user_data['images'] else None,
-                access_token=token_info['access_token'],
-                refresh_token=token_info['refresh_token'],
-                share_token=user_share_token,
-                token_expires_at=datetime.fromtimestamp(token_info['expires_at']),
-                last_updated=datetime.utcnow()
+        print("user instance made")
+        db.session.add(user)
+        try:
+            db.session.commit()
+            print("successfully commited to session")
+        except Exception as e:
+            # Handle database errors, such as connection issues or constraints violations
+            print(f"Unable to commit to session: {e}")
+            db.session.rollback()
+            return jsonify(error=str(e)), 500
+        
+        print("user instanced added to db session")
+
+        user_stats = UserStats(
+            user_id=user.id,
+            popularity_r=popularity_r,
+            popularity_a=popularity_a,
+            tempo_r=median_values_r[0],
+            tempo_a=median_values_a[0],
+            loudness_r=median_values_r[1],
+            loudness_a=median_values_a[1],
+            acousticness_r=median_values_r[2],
+            acousticness_a=median_values_a[2],
+            danceability_r=median_values_r[3],
+            danceability_a=median_values_a[3],
+            valence_r=median_values_r[4],
+            valence_a=median_values_a[4],
+            energy_r=median_values_r[5],
+            energy_a=median_values_a[5],
+            speechiness_r=median_values_r[6],
+            speechiness_a=median_values_a[6],
+            variance_r=variance_r,
+            variance_a=variance_a
             )
+        
+        db.session.add(user_stats)
+        try:
+            db.session.commit()
+            print("successfully commited to session")
+        except Exception as e:
+            # Handle database errors, such as connection issues or constraints violations
+            print(f"Unable to commit to session: {e}")
+            db.session.rollback()
+            return jsonify(error=str(e)), 500
+        print("User stats made and added to db session")
 
-            print("user instance made")
-            db.session.add(user)
-            try:
-                db.session.commit()
-            except Exception as e:
-                # Handle database errors, such as connection issues or constraints violations
-                print(f"Unable to commit to session: {e}")
-                db.session.rollback()
-                return jsonify(error=str(e)), 500
-            
-            print("user instanced added to db session")
-            
-            load_user_stats(user.id, median_values_r, median_values_a, new_data, update=False)
-        else:
-            # the user exists, update info in db
-            user.last_updated = datetime.utcnow()
-            load_user_stats(user.id, median_values_r, median_values_a, new_data, update=True)
-            
+        for g, count in genres_r.items():
+            new_genre = RecentGenres(
+                user_stats_id=user_stats.id,
+                genre=g,
+                genre_count=count
+            )
+            db.session.add(new_genre)
+
+        print("genres_r loaded in")
+        
+        for g, count in genres_a.items():
+            new_genre = AllTimeGenres(
+                user_stats_id=user_stats.id,
+                genre=g,
+                genre_count=count
+            )
+            db. session.add(new_genre)
+
+        print("genres_a loaded in")
+
+        for a in artists_r:
+            new_artist = RecentArtists(
+                user_stats_id=user_stats.id,
+                artist=a
+            )
+            db.session.add(new_artist)
+        
+        for a in artists_a:
+            new_artist = AllTimeArtists(
+                user_stats_id=user_stats.id,
+                artist=a
+            )
+            db. session.add(new_artist)
+        
+        print("artists loaded in")
+
+        print(tracks_r)
+        for track in tracks_r:
+            new_track = RecentTracks(
+                user_stats_id=user_stats.id,
+                song=track
+            )
+            db.session.add(new_track)
+        
+        for track in tracks_a:
+            new_track = AllTimeTracks(
+                user_stats_id=user_stats.id,
+                song=track
+            )
+            db. session.add(new_track)
+
         session['processed_data'] = {
-            "graph_json": {"median_values_r": median_values_r, "median_values_a": median_values_a},
-            "median_values_r": median_values_r,
-            "median_values_a": median_values_a,
-            "user_data": user_data,
-            "user_name": user_name,
-            "profile_pic": profile_pic,
-            "share_token": user.share_token,
-            "genre_r": genres_r,
-            "genre_a": genres_a,
-            "artists_a": artists_a,
-            "artists_r": artists_r,
-            "tracks_r": tracks_r,
-            "tracks_a": tracks_a,
-            "popularity_r": popularity_r,
-            "tempo_r": median_values_r[0],
-            "loudness_r": median_values_r[1],
-            "acousticness_r": median_values_r[2],
-            "danceability_r": median_values_r[3],
-            "valence_r": median_values_r[4],
-            "energy_r": median_values_r[5],
-            "speechiness_r": median_values_r[6],
-            "variance_r": variance_r,
-        }
+        "graph_json": {"median_values_r": median_values_r, "median_values_a": median_values_a},
+        "median_values_r": median_values_r,
+        "median_values_a": median_values_a,
+        "user_data": user_data,
+        "user_name": user_name,
+        "profile_pic": profile_pic,
+        "share_token": user_share_token,
+        "genre_r": genres_r,
+        "genre_a": genres_a,
+        "artists_a": artists_a,
+        "artists_r": artists_r,
+        "tracks_r": tracks_r,
+        "tracks_a": tracks_a,
+        "popularity_r": popularity_r,
+        "tempo_r": median_values_r[0],
+        "loudness_r": median_values_r[1],
+        "acousticness_r": median_values_r[2],
+        "danceability_r": median_values_r[3],
+        "valence_r": median_values_r[4],
+        "energy_r": median_values_r[5],
+        "speechiness_r": median_values_r[6],
+        "variance_r": variance_r,
+    }
 
     try:
         db.session.commit()
@@ -242,18 +295,6 @@ def fetch_data():
 
     # print(session['processed_data'])
     return jsonify(success=True)
-
-def cleanup():
-    cache_file = '.cache'
-    if os.path.exists(cache_file):
-        os.remove(cache_file)
-
-@app.route('/display')
-def display():
-    processed_data = session.get('processed_data', {})
-    if not processed_data:
-        return redirect('/login')
-    return render_template('user_dashboard.html', **processed_data)
 
 
 @app.route('/compare/<user1_share_token>/<user2_share_token>')
@@ -286,30 +327,25 @@ def compare_form():
     return render_template('compare_form.html')
 
 def get_graph_data(user_id):
+    # Fetch the user stats from the database
     user_stats = UserStats.query.filter_by(user_id=user_id).first()
+
+    # Check if user stats eaxist
     if not user_stats:
         return None
-    graph_data_all_time = {
-        'acousticness': user_stats.acousticness_a,
-        'danceability': user_stats.danceability_a,
-        'energy': user_stats.energy_a,
-        'valence': user_stats.valence_a,
-        'speechiness': user_stats.speechiness_a,
-        'popularity': user_stats.popularity_a,
-        'variance': user_stats.variance_a
+
+    # Prepare the graph data
+    graph_data = {
+        "acousticness": user_stats.acousticness_a,
+        "danceability": user_stats.danceability_a,
+        "valence": user_stats.valence_a,
+        "energy": user_stats.energy_a,
+        "speechiness": user_stats.speechiness_a,
+        "popularity": user_stats.popularity_a,
+        "variance": user_stats.variance_a
     }
-    graph_data_recent = {
-        'acousticness': user_stats.acousticness_r,
-        'danceability': user_stats.danceability_r,
-        'energy': user_stats.energy_r,
-        'valence': user_stats.valence_r,
-        'speechiness': user_stats.speechiness_r,
-        'popularity': user_stats.popularity_r,
-        'variance': user_stats.variance_r
-    }
-    print(graph_data_all_time)
-    print(graph_data_recent)
-    return graph_data_all_time, graph_data_recent
+    print(graph_data)
+    return graph_data
 
 @app.route('/comparison/<user1_share_token>/<user2_share_token>')
 def comparison(user1_share_token, user2_share_token):
@@ -317,25 +353,14 @@ def comparison(user1_share_token, user2_share_token):
     user2 = User.query.filter_by(share_token=user2_share_token).first()
     if not user1 or not user2:
         return jsonify({"error": "One or both users not found"}), 404
-    user1_graph_data_all_time, user1_graph_data_recent = get_graph_data(user1.id)
-    user2_graph_data_all_time, user2_graph_data_recent = get_graph_data(user2.id)
-    print(user1_graph_data_all_time)
-    print(user1_graph_data_recent)
-    print(user2_graph_data_all_time)
-    print(user2_graph_data_recent)
-
-    if not user1_graph_data_all_time or not user2_graph_data_all_time or not user1_graph_data_recent or not user2_graph_data_recent:
+    user1_graph_data = get_graph_data(user1.id)
+    user2_graph_data = get_graph_data(user2.id)
+    if not user1_graph_data or not user2_graph_data:
         return jsonify({"error": "Could not fetch graph data for one or both users"}), 500
-    return render_template('comparison.html', 
-                           user1_graph_data_all_time=user1_graph_data_all_time, 
-                           user1_graph_data_recent=user1_graph_data_recent, 
-                           user2_graph_data_all_time=user2_graph_data_all_time, 
-                           user2_graph_data_recent=user2_graph_data_recent, 
-                           user1_name=user1.display_name, 
-                           user2_name=user2.display_name)
+    return render_template('comparison.html', user1_graph_data=user1_graph_data, user2_graph_data=user2_graph_data)
 
 
-@app.route('/compare_users_redirect', methods=['POST'])
+@app.route('/compare_users', methods=['POST'])
 def compare_users_redirect():
     # Handle the form submission and redirect to the comparison using share tokens.
     user1_share_token = request.form['user1ShareToken']
@@ -343,13 +368,17 @@ def compare_users_redirect():
     return redirect(url_for('comparison', user1_share_token=user1_share_token, user2_share_token=user2_share_token))
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+def cleanup():
+    cache_file = '.cache'
+    if os.path.exists(cache_file):
+        os.remove(cache_file)
 
-@app.route('/select_playlist')
-def select_playlist():
-    return render_template('select_playlist.html')
+@app.route('/display')
+def display():
+    processed_data = session.get('processed_data', {})
+    if not processed_data:
+        return redirect('/login')
+    return render_template('user_dashboard.html', **processed_data)
 
 @app.route('/error')
 def error_page():

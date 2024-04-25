6812b1fe8ef69446ad288ce0b5a7aa7a3f3d5672
nmaffly <ncmaffly@ucdavis.edu>
2024-04-25 11:50:13 -0700

    minor changes to env configuration

--- app.py
+++ app.py
@@ -33,11 +33,9 @@ migrate = Migrate(app, db)
 
 def setup_spotify(team):
     print(f"Setting up Spotify for team {team}")
-    SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
-    SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+    SPOTIPY_CLIENT_ID = os.getenv(f'TEAM{team}_SPOTIPY_CLIENT_ID')
+    SPOTIPY_CLIENT_SECRET = os.getenv(f'TEAM{team}_SPOTIPY_CLIENT_SECRET')
     SPOTIPY_REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
-
-
 
     # Initialize Spotify auth with team-specific details
     auth_manager = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI,
